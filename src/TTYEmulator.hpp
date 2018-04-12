7fd159c26002e9af6a002a8fba8bf126e7233c04
rochajoel <rochajoel@gmail.com>
2018-04-12 22:26:56 +0100

    Added TTYEmulator.hpp

---
+++ src/TTYEmulator.hpp
@@ -0,0 +1,56 @@
+
+#ifndef JAR_TTYEMULATOR_HPP_
+#define JAR_TTYEMULATOR_HPP_
+
+#include <string>
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <cute/cute.h>
+
+namespace TestUnitCommon
+{
+	using namespace std;
+
+	class CTTYEmulator
+	{
+		int m_ttyFD;
+
+	public:
+		CTTYEmulator()
+		{
+			m_ttyFD = posix_openpt(O_RDWR);
+
+			unlockpt(m_ttyFD);
+		}
+		virtual ~CTTYEmulator()
+		{
+			close(m_ttyFD);
+		}
+
+		void send(const string data)
+		{
+			if (write(m_ttyFD, &data[0], data.size()) == -1)
+				FAILM("Could not write on tty!\n");
+		}
+
+		char* getSlaveTTYName()
+		{
+			return ptsname(m_ttyFD);
+		}
+
+		string readLine()
+		{
+			std::string line(2048, 0);
+			int readSize = -1;
+
+			if ((readSize = read(m_ttyFD, &line[0], line.size())) == -1)
+				throw ios_base::failure("Could not read from tty!\n");
+
+			return line.substr(0, readSize);
+		}
+	};
+}
+
+#endif /* JAR_TTYEMULATOR_HPP_ */
